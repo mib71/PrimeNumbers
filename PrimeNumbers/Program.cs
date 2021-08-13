933425d35a6c0b7ab718bded29e066ebafdac058
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-08-13 23:35:33 +0200

    Add project files.

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number you want to check: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int dividers = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    dividers++;
+                }
+            }
+
+            if (dividers == 2)
+            {
+                Console.WriteLine("The number is a Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("The number is not a Prime Number");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
